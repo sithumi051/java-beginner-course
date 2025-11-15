d99bbf3b52dcea19cb143b227eb2ae7550808c4e
Sithumi Hettiarachchi <113745@foe.sjp.ac.lk>
2025-11-15 17:49:45 +0530

    Add HelloWorld.java

---
+++ Hello_World.java
@@ -0,0 +1,5 @@
+class Example{   //Create a class named Example
+    public static void main(String[] args){    //Main method - starting point of the program
+        System.out.println("Hello World!");   //Print Hello World to the console
+    }
+}
