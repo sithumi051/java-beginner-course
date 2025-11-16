8686d515e814ff3f863f6487e71e8aec0614daf1
Sithumi Hettiarachchi <113745@foe.sjp.ac.lk>
2025-11-17 03:42:48 +0530

    Add second while loop program.

---
+++ WhileLoop2.java
@@ -0,0 +1,22 @@
+//package com.mycompany.whileloop2;
+
+public class WhileLoop2 {
+
+    public static void main(String[] args) {
+        int apple = 1;
+        while (apple < 20){
+            System.out.println(apple);
+            
+            apple +=2;
+        }
+        
+        System.out.println();
+        
+        int x = 10;
+        while (x>0){
+            System.out.println(x + " My name is Sithumi");
+            
+            x --;
+        }
+    }
+}
