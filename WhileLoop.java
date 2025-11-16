0c12d10f0cf74bc5e891cd3907a7d79c388da517
Sithumi Hettiarachchi <113745@foe.sjp.ac.lk>
2025-11-17 03:38:37 +0530

    Add while loop program.

---
+++ WhileLoop.java
@@ -0,0 +1,12 @@
+//package com.mycompany.whileloop;
+public class WhileLoop {
+
+    public static void main(String[] args) {
+            int count = 0;
+            
+            while (count<5){
+                System.out.println("This is my first NetBean project.");
+                count++;   //Otherwise it will run infinitely
+            }
+    }
+} 
